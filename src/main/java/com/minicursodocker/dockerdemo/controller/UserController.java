395c792aa39662a229ea12c218d57f1bcb0fb9a3
marcos alencar <marcos.12.01.07@gmail.com>
2024-11-25 17:45:21 -0300

    fix controller

--- src/main/java/com/minicursodocker/dockerdemo/controller/UserController.java
+++ src/main/java/com/minicursodocker/dockerdemo/controller/UserController.java
@@ -2,15 +2,14 @@ package com.minicursodocker.dockerdemo.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.minicursodocker.dockerdemo.dao.UserDaoImpl;
 import com.minicursodocker.dockerdemo.dao.UserDao;
 import com.minicursodocker.dockerdemo.entity.User;
 
-
-@Controller
+@RestController
 public class UserController {
 
     @GetMapping("/users")
